c6e067d4b30edd7de5a7da840d32428168e5f6c5
keerthanak-tw <76804118+keerthanak-tw@users.noreply.github.com>
2021-01-04 12:25:02 +0530

    Create PrimeFactors.java

---
+++ src/main/java/simplest/possible/implementation/PrimeFactors.java
@@ -0,0 +1,11 @@
+package simplest.possible.implementation;
+
+import java.util.*;
+
+public class PrimeFactors {
+    public List<Integer> factors = new ArrayList<Integer>();
+
+    public List<Integer> of(int number) {
+        return factors;
+    }
+}
